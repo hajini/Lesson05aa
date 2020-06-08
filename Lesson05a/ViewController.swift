570bb7c15f302e680bb9fed4f78f26a63c1ccd56
Jake <hajini0109@gmail.com>
2020-06-08 10:24:26 +0900

    Initial Commit

---
+++ Lesson05a/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson05a
+//
+//  Created by Hajin Jeong on 2020/06/08.
+//  Copyright © 2020 AmazingHajin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
